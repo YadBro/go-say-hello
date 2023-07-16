af6f09cb3222c1e12ee994df642a8af40634b8db
Yadi <yadi@millennia-solusi.id>
2023-07-16 11:50:17 +0700

    update sayhello v2

--- say_hello.go
+++ say_hello.go
@@ -3,5 +3,5 @@ package go_say_hello
 import "strconv"
 
 func SayHello(name string, age int) string {
-	return "Hello " + name + ", " + strconv.Itoa(age) + " years old.";
+	return "Hello " + name + ", " + strconv.Itoa(age) + " years old."
 }
